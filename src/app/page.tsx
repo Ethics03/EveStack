1e51f0ce38d591eb6a089ca939dfa4366fe3e183
Rachit Srivastava <honey032004@gmail.com>
2025-03-14 21:57:07 +0530

    feat: test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 'use client'
-
+import { Switch } from "@/components/ui/switch"
 export default function Home() {
   return (
+    <div className = "flex justify-around p-2">
     <h1>Arch btw.</h1>
+    <Switch/>
+    </div>
+
+    
   );
 }
