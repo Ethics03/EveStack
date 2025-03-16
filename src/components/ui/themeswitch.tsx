4734090fa51f9253a93b008e00e970bd7e0f3e04
Rachit Srivastava <honey032004@gmail.com>
2025-03-16 22:53:35 +0530

    feat: theme switch handled

---
+++ src/components/ui/themeswitch.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useTheme } from "next-themes"
+import { Children, useEffect, useState } from "react";
+import {Switch} from './switch'
+
+export default function ThemeSwitcher(){
+    const {theme,setTheme} = useTheme();
+
+    const [mt,setMt] = useState(false);
+
+    useEffect(()=> {
+        setMt(true);
+    },[])
+
+    if(!mt){
+        return null;
+    }
+    const handletheme = () => {
+        if(theme === 'light'){
+            setTheme("dark");
+        }
+        else{
+            setTheme("light");
+        }
+    }
+    return(
+        <div>
+            <Switch onClick={handletheme}/>
+        </div>
+    )
+}
