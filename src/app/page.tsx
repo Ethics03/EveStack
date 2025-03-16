2f325efb78e8e82b19dfc12028c244778c199e02
Rachit Srivastava <honey032004@gmail.com>
2025-03-16 23:10:49 +0530

    feat: logo change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@
 import ThemeSwitcher from "@/components/ui/themeswitch";
 export default function Home() {
   return (
-    <div className = "flex justify-around p-3">
-    <h1 className = 'font-bold text-xl'>EvePro</h1>
+    <div className = "flex items-center justify-around p-3 ">
+    <h1 className = 'font-bold text-xl'>EveStack</h1>
     <ThemeSwitcher/>
     </div>
 
