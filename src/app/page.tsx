349be65cac390f708e04a0fba7c6209c58325968
Rachit Srivastava <honey032004@gmail.com>
2025-03-16 22:53:45 +0530

    feat: added component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import { Switch } from "@/components/ui/switch"
+import ThemeSwitcher from "@/components/ui/themeswitch";
 export default function Home() {
   return (
     <div className = "flex justify-around p-3">
     <h1 className = 'font-bold text-xl'>EvePro</h1>
-    <Switch/>
+    <ThemeSwitcher/>
     </div>
 
     
