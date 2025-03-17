6742b0d6d99269916074d7fe24fd028a017a8c48
Rachit Srivastava <honey032004@gmail.com>
2025-03-17 22:46:55 +0530

    feat: added profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 'use client'
 
+import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
 import ThemeSwitcher from "@/components/ui/themeswitch";
 export default function Home() {
   return (
     <div className = "flex items-center justify-around p-3 ">
     <h1 className = 'font-bold text-xl'>EveStack</h1>
     <ThemeSwitcher/>
+    <NavigationMenu>
+  <NavigationMenuList>
+    <NavigationMenuItem>
+      <NavigationMenuTrigger>Profile</NavigationMenuTrigger>
+      <NavigationMenuContent>
+        <NavigationMenuLink>Link</NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  </NavigationMenuList>
+</NavigationMenu>
     </div>
 
     
